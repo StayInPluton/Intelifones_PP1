6b9cbdd1536bb462683a72321ccfdc5774377c4d
Lana <laisa0562@gmail.com>
2026-04-17 19:41:26 -0300

    first commit

---
+++ server/intelifones/src/main/java/br/com/ifpe/intelifones/IntelifonesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.intelifones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelifonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelifonesApplication.class, args);
+	}
+
+}
---
+++ server/intelifones/src/test/java/br/com/ifpe/intelifones/IntelifonesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.intelifones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelifonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
